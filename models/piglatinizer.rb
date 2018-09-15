44e19f4212682996fb6758518fa9a4b6460a1303
Nikki A <nikki.apostolakis@gmail.com>
2018-09-15 23:22:50 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,13 +7,13 @@ class PigLatinizer
 
   def piglatinize(phrase)
 
-    if phrase.split(/\W+/)
-      words = phrase.split(/\W+/)
-    else
+    if phrase.split(/\W+/) #if there is more than 1 word
+      words = phrase.split(/\W+/) #split up the words in the phrase
+    else #if the phrase is just one word
       words = phrase
     end
     phrase_converted = ""
-    words.each {|word| phrase_converted += piglatinize_single_word(word)}
+    words.each {|word| phrase_converted += piglatinize_single_word(word) + " "}
     phrase_converted
   end
   def piglatinize_single_word(word)
