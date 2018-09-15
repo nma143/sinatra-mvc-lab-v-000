a0b59ef244fcde097ca688c57273334c1a81ca58
Nikki A <nikki.apostolakis@gmail.com>
2018-09-15 22:25:57 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,9 +5,13 @@ class App < Sinatra::Base
   get '/' do
       erb :user_input
   end
-  
+
+  post '/' do
+      @converted_text = Piglatinizer.new(params[:user_text])
+      erb :results
+  end
 
 
 
-  
+
 end
