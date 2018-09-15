b54c27dfeef5b9197ab594c9aab9d4123cf65b1f
Nikki A <nikki.apostolakis@gmail.com>
2018-09-15 23:10:35 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,19 +7,24 @@ class PigLatinizer
 
   def piglatinize(text)
 
-    if text.match(/\A[aeiou]|[AIEOU]/) #if it starts with a vowel, just append 'way'
+    words = text.split(/\W+/)
+    text_converted = ""
+
+
+    end
+  end
+  def piglatinize_single_word(word)
+    word_converted=""
+    
+    if word.match(/\A[aeiou]|[AIEOU]/) #if it starts with a vowel, just append 'way'
       #text[0]=text[0].upcase!
-      text + "way"
+      word_converted += word + "way"
     else #else find the prefix (all letters before first vowel), append with 'ay'
-      parts = text.split /([aeiou].*)/ #=> e.g ["b", "anana"]
+      parts = word.split /([aeiou].*)/ #=> e.g ["b", "anana"]
       suffix = parts[0]+"ay"
       #parts[1][0].upcase
-      parts[1] + suffix
+      word_converted += parts[1] + suffix
     end
-
-
-
-
+    word_converted
   end
-
 end
