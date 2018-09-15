76cffdc72a35c1405f46bf5ec83e84d2c84ddabc
Nikki A <nikki.apostolakis@gmail.com>
2018-09-15 23:32:44 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,7 +7,7 @@ class PigLatinizer
 
   def piglatinize(phrase)
 
-    if phrase.split(/\W+/).length>0 #if there is more than 1 word
+    if phrase.split(/\W+/) #if there is more than 1 word
       words = phrase.split(/\W+/) #split up the words in the phrase
     else #if the phrase is just one word
       words = phrase
@@ -15,6 +15,7 @@ class PigLatinizer
     phrase_converted = ""
     wordcount=0
     words.each do |word|
+    {
       word_converted=""
       if word.match(/\A[aeiou]|[AIEOU]/) #if it starts with a vowel, just append 'way'
         #text[0]=text[0].upcase!
@@ -25,11 +26,12 @@ class PigLatinizer
         #parts[1][0].upcase
         word_converted += parts[1] + suffix
       end
-      if wordcount>0
+      if wordcount > 0
         word_converted = " " + word_converted
       end
       phrase_converted += word_converted
       ++wordcount
+    }
     end
     phrase_converted
   end
